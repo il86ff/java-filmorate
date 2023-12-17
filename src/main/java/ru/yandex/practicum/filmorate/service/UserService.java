0f75d319da3acd37d929fa4d05b6df96245ff181
il86ff <il86ff@gmail.com>
2023-12-17 16:30:36 +0300

    fix format

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -1,12 +1,9 @@
 package ru.yandex.practicum.filmorate.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.controller.FilmController;
 import ru.yandex.practicum.filmorate.exception.DuplicateIdException;
 import ru.yandex.practicum.filmorate.exception.IdIsNullException;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
